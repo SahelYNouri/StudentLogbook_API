741d1d7ff0332ea991286e24ce4644a72a07f6f3
Sahelyn-1006 <sahelnouri05@gmail.com>
2025-10-08 21:01:15 -0400

    changed formatting of the db session creaton

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 #all tables are created using the base class (just python classes that extend Base)
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
 #So in this project, instead of using psycopg2, ill use SQLAlchemy which is an ORM (object Relational Mapper)
 #An ORM allows us to interact with the databse using python objects instead of writing raw SQL queries like in psycopg2
@@ -20,11 +20,23 @@ password= os.getenv("PASSWORD")
 port = os.getenv("PORT")
 
 Base = declarative_base()
+
+#engine connects SQLALchemy to postgres db
 engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}")
 
 
 #use session to interact with db and commit, query, etc
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind= engine) #links session to db engine
+
+from sqlalchemy.orm import Session
+
+def get_db():
+    db = SessionLocal()  # create a new session
+    try:
+        yield db          # yield it to whatever endpoint needs it
+    finally:
+        db.close()        # close connection when request is done
+#dependency function to get db session for each request
 
 
 
